35ba30a8926c78c6c04a2d978574624e4d580bc5
raph941 <raph941@gmail.com>
2020-03-25 06:39:27 +0100

    deployment prep

--- sms/views.py
+++ sms/views.py
@@ -7,15 +7,13 @@ from twilio.twiml.messaging_response import MessagingResponse
 @csrf_exempt
 def sms_response(request):
     # Start our TwiML response
-    if request.method == 'POST':
+    request.method ='POST'
 
-        msg = request.POST['Body']
+    msg = request.POST.get('Body')
 
-        resp = MessagingResponse()
+    resp = MessagingResponse()
 
-        # Add a text message
-        resp.message(msg)
+    # Add a text message
+    resp.message(msg)
 
-        return HttpResponse(str(resp))
-    else:
-        pass
+    return HttpResponse(str(resp))
