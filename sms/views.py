8c6cff74c5de07f5cf2c69d7861bb43022c4af6c
raph941 <raph941@gmail.com>
2020-03-25 05:17:56 +0100

    initial commit

---
+++ sms/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from twilio.twiml.messaging_response import MessagingResponse
+
+
+@csrf_exempt
+def sms_response(request):
+    # Start our TwiML response
+    msg = request.POST['Body']
+
+    resp = MessagingResponse()
+
+    # Add a text message
+    resp.message(msg)
+
+    return HttpResponse(str(resp))
